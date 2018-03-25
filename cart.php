53275cf43a629bc88edaf4fc47cb52f8c42d41ad
Julien <julien.alexandre26@gmail.com>
2018-03-25 22:11:01 +0200

    Création du repository

---
+++ cart.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if (!isset($_SESSION['loginname'])) {
+    header('Location: login.php');
+    exit();
+}
+?>
+<?php require 'inc/head.php'; ?>
+<section class="cookies container-fluid">
+    <div class="row">
+        <table class="table">
+            <tr>
+                <th>Product</th>
+                <th>Quantity</th>
+            </tr>
+            <?php foreach ($_COOKIE['cart'] as $product => $quantity):?>
+            <tr>
+                <td><img src="assets/img/product-<?= $product ?>.jpg" alt="cookies choclate chips" class="img-responsive" style="height: 200px;"></td>
+                <td><?= $quantity ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
