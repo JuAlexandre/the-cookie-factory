42537ecdcfbd97d1fb21bbe187d9161d6d1e8b36
JuAlexandre <julien.alexandre26@gmail.com>
2019-02-02 14:07:16 +0100

    Add favicon

--- inc/head.php
+++ inc/head.php
@@ -15,6 +15,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>The Cookie Factory</title>
+    <link rel="icon" href="../img/favicon.ico" />
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" 
           href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
